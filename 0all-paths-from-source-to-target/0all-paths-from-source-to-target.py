44dde93f14f6398f7623dbe86de67614ef6e0afe
Hoai-An Phan <98496276+hoaian-phan@users.noreply.github.com>
2023-05-19 11:41:22 -0700

    Time: 108 ms, Memory: 18.3 MB - LeetHub

--- 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
+++ 0all-paths-from-source-to-target/0all-paths-from-source-to-target.py
@@ -1,16 +1,17 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        myStack = [(0, [0])]  # Each element in the stack is a tuple containing (current node, current path)
+        # The queue will store a list of (vertex, path)
+        queue = collections.deque([(0, [0])])
         paths = []
-
-        while myStack:
-            current, path = myStack.pop()
-
-            if current == len(graph) - 1:
+        
+        while queue:
+            node, path = queue.popleft()
+            if node == len(graph) - 1:
                 paths.append(path)
-
-            for neighbor in graph[current]:
-                myStack.append((neighbor, path + [neighbor]))
-
+            
+            for neighbor in graph[node]:
+                queue.append((neighbor, path + [neighbor]))
+                
         return paths
+                
                 
